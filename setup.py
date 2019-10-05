2a9d0fa220ad8157dac605b3837e50156737a696
Matthew Clapp <itsayellow+dev@gmail.com>
2019-10-05 16:44:59 -0700

    Use entry_points instead of scripts.

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup, find_packages
 
-setup(name='pyan',
-      setup_requires=['setuptools_scm'],
-      use_scm_version=True,
-      description="Offline call graph generator for Python 3",
-      packages=find_packages(),
-      include_package_data=True,
-      scripts=['scripts/pyan'],
-      zip_safe=False)
+setup(
+    name="pyan",
+    setup_requires=["setuptools_scm"],
+    use_scm_version=True,
+    description="Offline call graph generator for Python 3",
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={"console_scripts": ["pyan=pyan.pyan:main"]},
+    zip_safe=False,
+)
