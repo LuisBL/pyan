6e527a334eb38dbf265d114b1010be0312b64287
Matthew Clapp <itsayellow+dev@gmail.com>
2019-11-07 19:40:16 -0800

    Add url.

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     setup_requires=["setuptools_scm"],
     use_scm_version=True,
     description="Offline call graph generator for Python 3",
+    url="https://github.com/itsayellow/pyan",
     packages=find_packages(),
     include_package_data=True,
     entry_points={"console_scripts": ["pyan=pyan.pyan:main"]},
